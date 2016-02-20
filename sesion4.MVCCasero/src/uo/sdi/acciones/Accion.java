1244499a50d4d5477a29684367a0a7c13e5c7f3b
Sergio Moradas <sergimoradas@gmail.com>
2016-02-20 12:39:16 +0100

    Prueba Sergio

--- sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
+++ sesion4.MVCCasero/src/uo/sdi/acciones/Accion.java
@@ -3,6 +3,7 @@ package uo.sdi.acciones;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//Prueba
 public interface Accion {
 	
 	public String execute(HttpServletRequest request, HttpServletResponse response);
