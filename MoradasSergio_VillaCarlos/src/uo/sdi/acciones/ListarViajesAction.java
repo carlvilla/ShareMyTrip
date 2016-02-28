02a5a50af3c944e2e62fa287a60c76d12f9c4301
Carlos Villa <uo236851@uniovi.es>
2016-02-28 13:49:30 +0100

    Clase Sorter

--- MoradasSergio_VillaCarlos/src/uo/sdi/acciones/ListarViajesAction.java
+++ MoradasSergio_VillaCarlos/src/uo/sdi/acciones/ListarViajesAction.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import uo.sdi.acciones.util.Sorter;
 import uo.sdi.model.Application;
 import uo.sdi.model.Seat;
 import uo.sdi.model.Trip;
@@ -69,7 +70,8 @@ public class ListarViajesAction implements Accion {
 			
 			if(orden!=null){
 				
-				ordenarViajes(viajes,orden);
+				//Se llama al m
+				Sorter.ordenarViajes(viajes,orden);
 				
 			}
 
@@ -80,112 +82,6 @@ public class ListarViajesAction implements Accion {
 			Log.error("Algo ha ocurrido obteniendo lista de viajes");
 		}
 		return "EXITO";
-	}
-
-	/**
-	 * Ordena los viajes según el parámetro orden utilizando un aloritmo de
-	 * ordenación burbuja
-	 * 
-	 * @param viajes
-	 * @param orden
-	 */
-	private void ordenarViajes(List<Trip> viajes,String orden) {
-		 for(int i = 0; i < viajes.size() - 1; i++)
-	        {
-	            for(int j = 0; j < viajes.size() - 1; j++)
-	            {
-	                if (ordenacion(viajes.get(j),viajes.get(j + 1),orden))
-	                {
-	                    Trip tmp = viajes.get(j+1);
-	                    viajes.set(j+1,viajes.get(j));
-	                    viajes.set(j,tmp);
-	                }
-	            }
-	        }
-		
-	}
-
-	/**
-	 * 
-	 * Devuelve un boolean que indica que viaje de los dos pasados debe 
-	 * de ir antes en la lista
-	 * 
-	 * @param trip
-	 * @param trip2
-	 * @param orden
-	 * @return
-	 */
-	private boolean ordenacion(Trip trip, Trip trip2,String orden) {
-		
-		if(orden.equals("origen")){
-			
-			if(trip.getDeparture().getCity().compareTo(trip2.getDeparture().getCity())>0){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-			
-		}
-		
-		else if(orden.equals("destino")){
-			
-			if(trip.getDestination().getCity().compareTo(trip2.getDestination().getCity())>0){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-		}
-		
-		else if(orden.equals("fechaSalida")){
-			
-			if(trip.getDepartureDate().after(trip2.getDepartureDate())){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-		}
-		
-		else if(orden.equals("fechaLlegada")){
-			
-			if(trip.getArrivalDate().after(trip2.getArrivalDate())){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-		}
-		
-		else if(orden.equals("fechaFin")){
-			
-			if(trip.getClosingDate().after(trip2.getClosingDate())){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-		}
-		
-	else if(orden.equals("plazas")){
-			
-			if(trip.getAvailablePax()<(trip2.getAvailablePax())){
-				return true;
-			}
-			else{
-				return false;
-			}
-			
-		}
-		
-
-		return false;
 	}
 
 	private boolean comprobarRelacionViajeUsuario(Long idUsuario, Long idViaje) {
